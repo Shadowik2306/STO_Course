04cbb41796586c57656bf091e60c0ea0188705e6
shadowik <zahar.mark@bk.ru>
2023-04-05 23:46:32 +0400

    .

---
+++ STO_Course/STODatabaseImplement/Models/Spare.cs
@@ -0,0 +1,22 @@
+﻿using STODataModels.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STODatabaseImplement.Models
+{
+    public class Spare : ISpareModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = String.Empty;
+        [Required]
+        public double Price { get; set; }
+
+        
+    }
+}
