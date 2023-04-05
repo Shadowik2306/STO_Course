92be90e90576cc4a54390cff09dc5c26faa1020f
georgiy semikolenov <georgiy.semikolenov@mail.ru>
2023-04-05 23:47:06 +0400

    .

---
+++ STO_Course/STODatabaseImplement/Models/CarMaintenance.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace STODatabaseImplement.Models
+{
+    public class CarMaintenance
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int ReinforcedId { get; set; }
+
+        [Required]
+        public int ComponentId { get; set; }
+
+        public virtual Car Car { get; set; } = new();
+
+        public virtual Maintenance Maintenance { get; set; } = new();
+    }
+}
---
+++ STO_Course/STODatabaseImplement/Models/Maintenance.cs
@@ -0,0 +1,22 @@
+﻿using STODataModels.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace STODatabaseImplement.Models
+{
+    public class Maintenance : IMaintenanceModel
+    {
+        public int Id { get; set; }
+        [Required]
+        public int EmployerId { get; set; }
+        [Required]
+        public double Cost { get; set; }
+        [Required]
+        public DateTime DateCreate { get; set; }
+
+        private Dictionary<int, (ICarModel, int)> _maintenanceCars = null;
+        [NotMapped]
+        public Dictionary<int, (ICarModel, int)> MaintenanceCars => throw new NotImplementedException();
+        
+    }
+}
