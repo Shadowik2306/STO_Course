06d6228844c9270590cfd5bf831a04599a048157
georgiy semikolenov <georgiy.semikolenov@mail.ru>
2023-04-07 00:32:47 +0400

    .

--- STO_Course/STODatabaseImplement/Models/Employer.cs
+++ STO_Course/STODatabaseImplement/Models/Employer.cs
@@ -1,12 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using STOContracts.BindingModels;
+using STOContracts.ViewModels;
+using STODataModels.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace STODatabaseImplement.Models
 {
-    internal class Employer
+    public class Employer : IEmployerModel
     {
+        public int Id { get; set; }
+        [Required]
+        public string Login { get; set; } = String.Empty;
+        [Required]
+        public string Password { get; set; } = String.Empty;
+        [Required]
+        public string Email { get; set; } = String.Empty;
+        [ForeignKey("EmployerId")]
+        public virtual List<Maintenance> Maintenances { get; set; } = new();
+
+
+        public static Employer Create(StorekeeperBindingModel model)
+        {
+            return new Employer()
+            {
+                Id = model.Id,
+                Login = model.Login,
+                Password = model.Password,
+                Email = model.Email,
+            };
+        }
+
+        public void Update(EmployerBindingModel model)
+        {
+            Login = model.Login;
+            Password = model.Password;
+            Email = model.Email;
+        }
+
+        public EmployerViewModel GetViewModel => new()
+        {
+            Id = Id,
+            Login = Login,
+            Email = Email,
+            Password = Password
+        };
     }
 }
