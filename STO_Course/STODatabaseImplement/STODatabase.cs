b7d13d14c0fabfb36d8b723d301e0a5f05f8cb4d
georgiy semikolenov <georgiy.semikolenov@mail.ru>
2023-04-07 02:16:21 +0400

    .

--- STO_Course/STODatabaseImplement/STODatabase.cs
+++ STO_Course/STODatabaseImplement/STODatabase.cs
@@ -10,16 +10,20 @@ namespace STODatabaseImplement
         {
             if (!optionsBuilder.IsConfigured)
             {
-				optionsBuilder.UseSqlServer(@"Data Source=PREMIX\SQLEXPRESS03;Initial Catalog=STO;Integrated Security=True;TrustServerCertificate=True"); 
+				optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-QVK6NSA\SQLEXPRESS03;Initial Catalog=STO;Integrated Security=True;TrustServerCertificate=True"); 
 			}
             base.OnConfiguring(optionsBuilder);
         }
         public virtual DbSet<Car> Cars { set; get; }
         public virtual DbSet<CarSpare> CarSpares { set; get; }
+        public virtual DbSet<Employer> Employers { set; get; }
         public virtual DbSet<Maintenance> Maintenances { set; get; }
         public virtual DbSet<MaintenanceCar> MaintenanceCars { set; get; }
+        public virtual DbSet<Service> Services { set; get; }
         public virtual DbSet<Spare> Spares { set; get; }
+        public virtual DbSet<Storekeeper> Storekeepers  { set; get; }
         public virtual DbSet<Work> Works { set; get; }
+        public virtual DbSet<WorkDuration> WorkDurations { set; get; }
         public virtual DbSet<WorkMaintence> WorkMaintences { set; get; }
         public virtual DbSet<WorkSpare> WorkSpares { set; get; }
 
