e1d016a2e808cb72bd9ba4da4e5cfe99672dc823
shadowik <zahar.mark@bk.ru>
2023-05-21 23:41:26 +0400

    .

--- STO_Course/STOEmployer/Controllers/HomeController.cs
+++ STO_Course/STOEmployer/Controllers/HomeController.cs
@@ -1,4 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using STOBusinessLogic.BusinessLogics;
+using STOContracts.BindingModels;
+using STOContracts.BusinessLogicsContracts;
+using STOContracts.SearchModels;
+using STOContracts.ViewModels;
+using STODataModels.Models;
 using STOEmployer.Models;
 using System.Diagnostics;
 
@@ -8,25 +14,69 @@ namespace STOEmployer.Controllers
 	{
 		private readonly ILogger<HomeController> _logger;
 
-		public HomeController(ILogger<HomeController> logger)
+        public static EmployerViewModel? Employer { get; set; } = null;
+
+		public readonly IEmployerLogic _employerLogic;
+		public readonly IMaintenanceLogic _maintenanceLogic;
+		public readonly ICarLogic _carLogic;
+		public readonly IServiceLogic _serviceLogic;
+
+		public readonly IStorekeeperLogic _storekeeperLogic;
+		public readonly IWorkLogic _workLogic;
+		public readonly IWorkDurationLogic _workDurationLogic;
+		public readonly ISpareLogic _spareLogic;
+
+		public HomeController(ILogger<HomeController> logger, IEmployerLogic employerLogic, IMaintenanceLogic maintenanceLogic,
+			ICarLogic carLogic, IServiceLogic serviceLogic, IStorekeeperLogic storekeeperLogic,
+			IWorkDurationLogic workDurationLogic, ISpareLogic spareLogic, IWorkLogic workLogic)
 		{
 			_logger = logger;
+			_employerLogic = employerLogic;
+			_spareLogic = spareLogic;
+			_carLogic = carLogic;
+			_serviceLogic = serviceLogic;
+			_storekeeperLogic = storekeeperLogic;
+			_maintenanceLogic = maintenanceLogic;
+			_workDurationLogic= workDurationLogic;
+			_workLogic = workLogic;
 		}
 
-		public IActionResult Index()
-		{
+		[HttpGet]
+		public IActionResult Privacy() { 
 			return View();
 		}
 
-		public IActionResult Privacy()
-		{
+		[HttpGet]
+		public IActionResult Register() {
+			return View(Employer);
+		}
+
+		[HttpPost]
+		public IActionResult Register(string login, string email, string password) {
+			_employerLogic.Create(new EmployerBindingModel()
+			{
+				Login = login,
+				Email = email,
+				Password = password
+			});
+
+			return Redirect("Privacy");
+		}
+
+		[HttpGet]
+		public IActionResult Enter() {
 			return View();
 		}
 
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public IActionResult Error()
-		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		[HttpPost]
+		public IActionResult Enter(string login, string password) {
+			Employer = _employerLogic.ReadElement(new EmployerSearchModel()
+			{
+				Login = login,
+				Password = password,
+			});
+
+			return Redirect("Privacy");
 		}
 	}
 }
