e6ba0b1251ceb8e2cc92d504fb800ef115cb65c1
georgiy semikolenov <georgiy.semikolenov@mail.ru>
2023-05-21 23:16:08 +0400

    .

---
+++ STO_Course/STOEmployer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace STOEmployer.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
