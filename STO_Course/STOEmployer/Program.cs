94c309947bf5f072ebbd66642831794670353ee9
georgiy semikolenov <georgiy.semikolenov@mail.ru>
2023-05-22 00:16:53 +0400

    buid

--- STO_Course/STOEmployer/Program.cs
+++ STO_Course/STOEmployer/Program.cs
@@ -1,7 +1,30 @@
+using STOBusinessLogic.BusinessLogics;
+using STOContracts.BusinessLogicsContracts;
+using STOContracts.StoragesContracts;
+using STODatabaseImplement.Implements;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddTransient<ICarStorage, CarStorage>();
+builder.Services.AddTransient<IEmployerStorage, EmployerStorage>();
+builder.Services.AddTransient<IMaintenanceStorage, MaintenanceStorage>();
+builder.Services.AddTransient<IServiceStorage, ServiceStorage>();
+builder.Services.AddTransient<ISpareStorage, SpareStorage>();
+builder.Services.AddTransient<IStorekeeperStorage, StorekeeperStorage>();
+builder.Services.AddTransient<IWorkDurationStorage, WorkDurationStorage>();
+builder.Services.AddTransient<IWorkStorage, WorkStorage>();
+
+builder.Services.AddTransient<ICarLogic, CarLogic>();
+builder.Services.AddTransient<IEmployerLogic, EmployerLogic>();
+builder.Services.AddTransient<IMaintenanceLogic, MaintenanceLogic>();
+builder.Services.AddTransient<IServiceLogic, ServiceLogic>();
+builder.Services.AddTransient<ISpareLogic, SpareLogic>();
+builder.Services.AddTransient<IStorekeeperLogic, StorekeeperLogic>();
+builder.Services.AddTransient<IWorkDurationLogic, WorkDurationLogic>();
+builder.Services.AddTransient<IWorkLogic, WorkLogic>();
 
 var app = builder.Build();
 
