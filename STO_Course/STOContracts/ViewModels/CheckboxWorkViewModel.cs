32d8708c8d42e94a78980901ce4b93bf2d765271
shadowik <zahar.mark@bk.ru>
2023-05-22 19:36:32 +0400

    .

---
+++ STO_Course/STOContracts/ViewModels/CheckboxWorkViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STOContracts.ViewModels
+{
+    public class CheckboxWorkViewModel
+    {
+        public List<CheckboxViewModel> Spares { get; set; }
+
+        public List<CheckboxViewModel> Maintenance { get; set; }
+    }
+}
